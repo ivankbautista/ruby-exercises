fafff1f51a7cff2ca3654538dc20c168fe9ea0b6
Ivan Bautista <ivankevinbautista@gmail.com>
2021-09-24 19:39:23 +0800

    Add in answer for isograms exercise

---
+++ data-types/9.24.2021/isograms.rb
@@ -0,0 +1,13 @@
+# An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+# is_isogram("Dermatoglyphics" ) == true
+# is_isogram("aba" ) == false
+# is_isogram("moOse" ) == false # -- ignore letter case
+
+def is_isogram(string)
+    lower_string = string.downcase
+    ('a'..'z').each do |letters|
+      return false if lower_string.count(letters) > 1
+    end
+    return true
+  end
